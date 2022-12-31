5e0c06a1dc42d7f240f7d1a503e14d340c0c34d5
Enric Tobella <etobella@creublanca.es>
2022-12-31 10:49:11 +0100

    [FIX] spreadsheet_dashboard_oca

--- spreadsheet_dashboard_oca/models/spreadsheet_dashboard.py
+++ spreadsheet_dashboard_oca/models/spreadsheet_dashboard.py
@@ -1,11 +1,19 @@
 # Copyright 2022 CreuBlanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+import base64
+
+from odoo import fields, models
 
 
 class SpreadsheetDashboard(models.Model):
     _inherit = "spreadsheet.dashboard"
+
+    raw = fields.Binary(inverse="_inverse_raw")
+
+    def _inverse_raw(self):
+        for record in self:
+            record.data = base64.encodebytes(record.raw)
 
     def open_spreadsheet(self):
         self.ensure_one()
