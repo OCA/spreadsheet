eb7fecf3dc7647f3df39ffef7b9a7dddc3ef391c
Enric Tobella <etobella@creublanca.es>
2023-01-02 00:07:56 +0100

    [IMP] spreadsheet_oca: Pass all logic to an abstract

---
+++ spreadsheet_oca/models/spreadsheet_abstract.py
@@ -0,0 +1,28 @@
+# Copyright 2022 CreuBlanca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+from odoo import fields, models
+
+
+class SpreadsheetAbstract(models.AbstractModel):
+    _name = "spreadsheet.abstract"
+    _description = "Spreadsheet abstract for inheritance"
+
+    name = fields.Char()
+    raw = fields.Binary()
+
+    def open_spreadsheet(self):
+        self.ensure_one()
+        return {
+            "type": "ir.actions.client",
+            "tag": "action_spreadsheet_oca",
+            "params": {"spreadsheet_id": self.id, "model": self._name},
+        }
+
+    def send_spreadsheet_message(self, message):
+        self.ensure_one()
+        channel = "spreadsheet_oca;%s;%s" % (self._name, self.id)
+        message.update({"res_model": self._name, "res_id": self.id})
+        self.env["bus.bus"]._sendone(channel, "spreadsheet_oca", message)
+        return True
