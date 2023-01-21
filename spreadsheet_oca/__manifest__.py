4a7c22a28ad70494119de92340bec64cfc7cb7c5
Enric Tobella <etobella@creublanca.es>
2023-01-22 00:17:24 +0100

    [IMP] spreadsheet_oca: Allow to export graphs to spreadsheets

--- spreadsheet_oca/__manifest__.py
+++ spreadsheet_oca/__manifest__.py
@@ -23,6 +23,7 @@
             "spreadsheet_oca/static/src/spreadsheet/spreadsheet.xml",
             "spreadsheet_oca/static/src/spreadsheet/spreadsheet_action.esm.js",
             "spreadsheet_oca/static/src/spreadsheet/pivot_controller.esm.js",
+            "spreadsheet_oca/static/src/spreadsheet/graph_controller.esm.js",
         ],
         "spreadsheet.o_spreadsheet": [
             "spreadsheet_oca/static/src/spreadsheet/bundle/spreadsheet.xml",
