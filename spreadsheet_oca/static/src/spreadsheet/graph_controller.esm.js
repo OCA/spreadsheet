c526be6f4d9a643bb072c979c6949680161a2410
Enric Tobella <etobella@creublanca.es>
2023-01-22 00:17:24 +0100

    [IMP] spreadsheet_oca: Allow to export graphs to spreadsheets

---
+++ spreadsheet_oca/static/src/spreadsheet/graph_controller.esm.js
@@ -0,0 +1,28 @@
+/** @odoo-module **/
+import {GraphController} from "@web/views/graph/graph_controller";
+
+import {patch} from "web.utils";
+
+patch(
+    GraphController.prototype,
+    "spreadsheet_oca/static/src/spreadsheet/graph_controller.esm.js",
+    {
+        onSpreadsheetButtonClicked() {
+            this.actionService.doAction(
+                "spreadsheet_oca.spreadsheet_spreadsheet_import_act_window",
+                {
+                    additionalContext: {
+                        default_name: this.model.metaData.title,
+                        default_import_data: {
+                            mode: "graph",
+                            metaData: JSON.parse(JSON.stringify(this.model.metaData)),
+                            searchParams: JSON.parse(
+                                JSON.stringify(this.model.searchParams)
+                            ),
+                        },
+                    },
+                }
+            );
+        },
+    }
+);
