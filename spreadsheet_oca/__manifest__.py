88c4b6f41569f3295ced13b16458f4c6d8b1d601
Enric Tobella <etobella@creublanca.es>
2023-07-09 23:57:36 +0200

    [IMP] spreadsheet_oca: Allow to edit Odoo graphs properly
    Fixes #9

--- spreadsheet_oca/__manifest__.py
+++ spreadsheet_oca/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Spreadsheet Oca",
     "summary": """
         Allow to edit spreadsheets""",
-    "version": "16.0.1.1.2",
+    "version": "16.0.1.1.3",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/spreadsheet",
@@ -34,6 +34,7 @@
             "spreadsheet_oca/static/src/spreadsheet/bundle/spreadsheet_renderer.esm.js",
             "spreadsheet_oca/static/src/spreadsheet/bundle/spreadsheet_controlpanel.esm.js",
             "spreadsheet_oca/static/src/spreadsheet/bundle/spreadsheet_action.esm.js",
+            "spreadsheet_oca/static/src/spreadsheet/bundle/odoo_panels.esm.js",
         ],
     },
 }
