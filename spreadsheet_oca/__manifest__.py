440ca35d7dd340babdedced53e454cf1ee87cd13
Nguyễn Đại Dương <daiduongnguyen2709@gmail.com>
2024-05-02 16:21:19 +0700

    [FIX] spreadsheet_oca: error when adding relation filter
    Close https://github.com/OCA/spreadsheet/issues/24 and
    https://github.com/OCA/spreadsheet/issues/27

--- spreadsheet_oca/__manifest__.py
+++ spreadsheet_oca/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Spreadsheet Oca",
     "summary": """
         Allow to edit spreadsheets""",
-    "version": "16.0.1.2.1",
+    "version": "16.0.1.2.3",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/spreadsheet",
