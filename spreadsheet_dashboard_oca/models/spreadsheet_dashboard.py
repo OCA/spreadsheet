b8f5e749046f7f1cbdf8ecfbd8eb1ec06aeaa967
Carlos Roca <carlos.roca@tecnativa.com>
2024-07-17 12:20:58 +0200

    [FIX] spreadsheet_dashboard_oca: Error with dashboard_sheet selected
    The error is happening because the dashboard where
    the data has to be added is not being showed.
    
    With this changes the field to define the
    dashboard is required and is showed.
    
    Added a way to limit the search to the dashboards
    that can be edited

--- spreadsheet_dashboard_oca/models/spreadsheet_dashboard.py
+++ spreadsheet_dashboard_oca/models/spreadsheet_dashboard.py
@@ -4,7 +4,8 @@
 import base64
 import json
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class SpreadsheetDashboard(models.Model):
@@ -15,7 +16,7 @@ class SpreadsheetDashboard(models.Model):
     spreadsheet_raw = fields.Serialized(
         inverse="_inverse_spreadsheet_raw", compute="_compute_spreadsheet_raw"
     )
-    can_edit = fields.Boolean(compute="_compute_can_edit")
+    can_edit = fields.Boolean(compute="_compute_can_edit", search="_search_can_edit")
 
     @api.depends("data")
     def _compute_spreadsheet_raw(self):
@@ -42,3 +43,24 @@ class SpreadsheetDashboard(models.Model):
             )
             if imd and imd.module != "__export__":
                 record.can_edit = imd.noupdate
+
+    @api.model
+    def _search_can_edit(self, operator, value):
+        if operator != "=":
+            raise NotImplementedError(_("Search operation not supported"))
+        if not isinstance(value, bool):
+            raise ValidationError(_("The value has to be a boolean"))
+        no_edit_ids = (
+            self.env["ir.model.data"]
+            .search(
+                [
+                    ("model", "=", self._name),
+                    ("module", "!=", "__export__"),
+                    ("noupdate", "=", 0),
+                ]
+            )
+            .mapped("res_id")
+        )
+        if value:
+            return [("id", "not in", no_edit_ids)]
+        return [("id", "in", no_edit_ids)]
