04a2d0d2d195a723a2be4f7e7a21ff1c4a49cf6e
Carlos Roca <carlos.roca@tecnativa.com>
2024-07-17 12:19:14 +0200

    [FIX+IMP] spreadsheet_dashboard_oca: Define data name + error on dashboard_sheet + dynamic + new dashboard
    - Add field to define name of table and restructure order of fields
    
    - Error with dashboard_sheet selected
    
    The error is happening because the dashboard where
    the data has to be added is not being showed.
    
    With this changes the field to define the
    dashboard is required and is showed.
    
    Added a way to limit the search to the dashboards
    that can be edited
    
    - Allow to generate dynamic tables
    
    On dashboards exists tables that are updated dynamically,
    with this changes we have the option to generate this
    dynamic tables using the pivot inserter.
    
    Added a button to duplicate and change to dynamic
    table the selected table on spreadsheet editor.
    
    If a table with multiple row goups generated, the
    way to generate the dynamic table will be set the
    number of rows for each level of indentation.
    
    Example:
    2 rowGroupBys
    3 number of rows
    - val1
        - subval1.1
        - subval1.2
        - subval1.3
    - val2
        - subval2.1
        - subval2.2
        - subval2.3
    - val3
        - subval3.1
        - subval3.2
        - subval3.3
    
    - Add option to create new dashboard

--- spreadsheet_dashboard_oca/wizards/spreadsheet_spreadsheet_import.py
+++ spreadsheet_dashboard_oca/wizards/spreadsheet_spreadsheet_import.py
@@ -9,11 +9,39 @@ class SpreadsheetSpreadsheetImport(models.TransientModel):
     _inherit = "spreadsheet.spreadsheet.import"
 
     dashboard_id = fields.Many2one("spreadsheet.dashboard")
+    dashboard_group_id = fields.Many2one("spreadsheet.dashboard.group")
+
+    def _create_spreadsheet_dashboard_vals(self):
+        return {
+            "name": self.name,
+            "dashboard_group_id": self.dashboard_group_id.id,
+        }
+
+    def _insert_pivot_dashboard_spreadsheet(self):
+        dashboard = self.env["spreadsheet.dashboard"].create(
+            self._create_spreadsheet_dashboard_vals()
+        )
+        import_data = self.import_data
+        import_data["name"] = self.datasource_name
+        import_data["new"] = 1
+        if self.dynamic:
+            import_data["dyn_number_of_rows"] = self.number_of_rows
+        return {
+            "type": "ir.actions.client",
+            "tag": "action_spreadsheet_oca",
+            "params": {
+                "model": dashboard._name,
+                "spreadsheet_id": dashboard.id,
+                "import_data": import_data,
+            },
+        }
 
     def _insert_pivot_dashboard(self, new_sheet=False):
         import_data = self.import_data
-        import_data["name"] = self.name
+        import_data["name"] = self.datasource_name
         import_data["new_sheet"] = new_sheet
+        if self.dynamic:
+            import_data["dyn_number_of_rows"] = self.number_of_rows
         return {
             "type": "ir.actions.client",
             "tag": "action_spreadsheet_oca",
