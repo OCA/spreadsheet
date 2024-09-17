7b555765776dd468bc87e562f62a36118a3b39d3
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2024-09-17 12:40:51 +0530

    [IMP] spreadsheet_dashboard_oca: black, isort, prettier

--- spreadsheet_dashboard_oca/wizards/spreadsheet_spreadsheet_import.py
+++ spreadsheet_dashboard_oca/wizards/spreadsheet_spreadsheet_import.py
@@ -5,7 +5,6 @@ from odoo import fields, models
 
 
 class SpreadsheetSpreadsheetImport(models.TransientModel):
-
     _inherit = "spreadsheet.spreadsheet.import"
 
     dashboard_id = fields.Many2one("spreadsheet.dashboard")
