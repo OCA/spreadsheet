c509e2ee8efe8c957c4ffa9ba7fa50e01bbaf303
Luis Rodriguez <luis.rodriguez@dixmit.com>
2025-01-08 12:06:16 +0100

    [IMP] spreadsheet_oca: pre-commit auto fixes

--- spreadsheet_oca/static/src/spreadsheet/bundle/odoo_panels.esm.js
+++ spreadsheet_oca/static/src/spreadsheet/bundle/odoo_panels.esm.js
@@ -10,67 +10,67 @@ const {LineBarPieDesignPanel} = spreadsheet.components;
 const {Component} = owl;
 
 export class OdooPanel extends Component {
-    setup() {
-        this.menus = useService("menu");
+  setup() {
+    this.menus = useService("menu");
+  }
+  get menuId() {
+    const menu = this.env.model.getters.getChartOdooMenu(this.props.figureId);
+    if (menu) {
+      return [menu.id, menu.name];
     }
-    get menuId() {
-        const menu = this.env.model.getters.getChartOdooMenu(this.props.figureId);
-        if (menu) {
-            return [menu.id, menu.name];
-        }
-        return false;
+    return false;
+  }
+  updateMenu(menuId) {
+    if (!menuId) {
+      this.env.model.dispatch("LINK_ODOO_MENU_TO_CHART", {
+        chartId: this.props.figureId,
+        odooMenuId: false,
+      });
+      return;
     }
-    updateMenu(menuId) {
-        if (!menuId) {
-            this.env.model.dispatch("LINK_ODOO_MENU_TO_CHART", {
-                chartId: this.props.figureId,
-                odooMenuId: false,
-            });
-            return;
-        }
-        const menu = this.env.model.getters.getIrMenu(menuId[0]);
-        this.env.model.dispatch("LINK_ODOO_MENU_TO_CHART", {
-            chartId: this.props.figureId,
-            odooMenuId: menu.xmlid || menu.id,
-        });
-    }
-    get record() {
-        const menus = this.menus
-            .getAll()
-            .map((menu) => menu.id)
-            .filter((menuId) => menuId !== "root");
-        return {
-            getFieldDomain: function () {
-                return new Domain([["id", "in", menus]]);
-            },
-            getFieldContext: function () {
-                return {};
-            },
-        };
-    }
+    const menu = this.env.model.getters.getIrMenu(menuId[0]);
+    this.env.model.dispatch("LINK_ODOO_MENU_TO_CHART", {
+      chartId: this.props.figureId,
+      odooMenuId: menu.xmlid || menu.id,
+    });
+  }
+  get record() {
+    const menus = this.menus
+      .getAll()
+      .map((menu) => menu.id)
+      .filter((menuId) => menuId !== "root");
+    return {
+      getFieldDomain: function () {
+        return new Domain([["id", "in", menus]]);
+      },
+      getFieldContext: function () {
+        return {};
+      },
+    };
+  }
 }
 OdooPanel.template = "spreadsheet_oca.OdooPanel";
 OdooPanel.components = {Many2OneField};
 
 class OdooStackablePanel extends OdooPanel {
-    onChangeStacked(ev) {
-        this.props.updateChart(this.props.figureId, {
-            stacked: ev.target.checked,
-        });
-    }
+  onChangeStacked(ev) {
+    this.props.updateChart(this.props.figureId, {
+      stacked: ev.target.checked,
+    });
+  }
 }
 OdooStackablePanel.template = "spreadsheet_oca.OdooStackablePanel";
 
 chartSidePanelComponentRegistry
-    .add("odoo_line", {
-        configuration: OdooStackablePanel,
-        design: LineBarPieDesignPanel,
-    })
-    .add("odoo_bar", {
-        configuration: OdooStackablePanel,
-        design: LineBarPieDesignPanel,
-    })
-    .add("odoo_pie", {
-        configuration: OdooPanel,
-        design: LineBarPieDesignPanel,
-    });
+  .add("odoo_line", {
+    configuration: OdooStackablePanel,
+    design: LineBarPieDesignPanel,
+  })
+  .add("odoo_bar", {
+    configuration: OdooStackablePanel,
+    design: LineBarPieDesignPanel,
+  })
+  .add("odoo_pie", {
+    configuration: OdooPanel,
+    design: LineBarPieDesignPanel,
+  });
--- spreadsheet_oca/static/src/spreadsheet/spreadsheet_action.esm.js
+++ spreadsheet_oca/static/src/spreadsheet/spreadsheet_action.esm.js
@@ -6,16 +6,12 @@ import {registry} from "@web/core/registry";
 const actionRegistry = registry.category("actions");
 
 const loadSpreadsheetActionOca = async (env, context) => {
-    await loadSpreadsheetAction(
-        env,
-        "action_spreadsheet_oca",
-        loadSpreadsheetActionOca
-    );
-    return {
-        ...context,
-        target: "current",
-        tag: "action_spreadsheet_oca",
-        type: "ir.actions.client",
-    };
+  await loadSpreadsheetAction(env, "action_spreadsheet_oca", loadSpreadsheetActionOca);
+  return {
+    ...context,
+    target: "current",
+    tag: "action_spreadsheet_oca",
+    type: "ir.actions.client",
+  };
 };
 actionRegistry.add("action_spreadsheet_oca", loadSpreadsheetActionOca);
