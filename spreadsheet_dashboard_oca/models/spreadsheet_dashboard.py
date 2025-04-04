8ce445db40fe0290a6a0631d444b34de83a45b94
Chris Mann <chrisandrewmann@gmail.com>
2025-04-04 16:42:27 +0100

    [IMP] Remove dependency on spreadsheet.mixin, moved to spreadsheet.abstract

--- spreadsheet_dashboard_oca/models/spreadsheet_dashboard.py
+++ spreadsheet_dashboard_oca/models/spreadsheet_dashboard.py
@@ -1,41 +1,16 @@
 # Copyright 2022 CreuBlanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-import base64
-import json
-
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class SpreadsheetDashboard(models.Model):
     _name = "spreadsheet.dashboard"
-    _inherit = ["spreadsheet.dashboard", "spreadsheet.abstract", "spreadsheet.mixin"]
+    _inherit = ["spreadsheet.dashboard", "spreadsheet.abstract"]
 
-    spreadsheet_binary_data = fields.Binary()
     active = fields.Boolean(default=True)
-    spreadsheet_raw = fields.Serialized(
-        inverse="_inverse_spreadsheet_raw", compute="_compute_spreadsheet_raw"
-    )
     can_edit = fields.Boolean(compute="_compute_can_edit", search="_search_can_edit")
-
-    @api.depends("spreadsheet_binary_data")
-    def _compute_spreadsheet_raw(self):
-        for dashboard in self:
-            if dashboard.spreadsheet_binary_data:
-                dashboard.spreadsheet_raw = json.loads(
-                    base64.decodebytes(dashboard.spreadsheet_binary_data).decode(
-                        "UTF-8"
-                    )
-                )
-            else:
-                dashboard.spreadsheet_raw = {}
-
-    def _inverse_spreadsheet_raw(self):
-        for record in self:
-            record.spreadsheet_binary_data = base64.encodebytes(
-                json.dumps(record.spreadsheet_raw).encode("UTF-8")
-            )
 
     def _compute_can_edit(self):
         """We can edit if the record doesn't have XML-ID, or the XML-ID is noupdate=1"""
