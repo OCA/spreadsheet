5fc26a4cee09be9f90cc0676a2f56160ab786375
Odoo Wired <odoowired@erp-m.eu>
2025-07-30 11:27:45 +0200

    [MIG] spreadsheet_dashboard_oca: Migration to 18.0

--- spreadsheet_dashboard_oca/__manifest__.py
+++ spreadsheet_dashboard_oca/__manifest__.py
@@ -5,14 +5,13 @@
     "name": "Spreadsheet Dashboard Oca",
     "summary": """
         Use OCA Spreadsheets on dashboards configuration""",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/spreadsheet",
     "depends": [
         "spreadsheet_dashboard",
         "spreadsheet_oca",
-        # "base_view_inheritance_extension",
     ],
     "data": [
         "wizards/spreadsheet_spreadsheet_import.xml",
