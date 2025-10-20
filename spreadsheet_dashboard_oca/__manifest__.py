7a6a961923148324f492a0d9502ed024655ddaea
Chris Mann <chrisandrewmann@gmail.com>
2025-10-20 11:42:57 +0100

    [IMP] spreadsheet_dashboard_oca: Add to dashboard feature from existing spreadsheet

--- spreadsheet_dashboard_oca/__manifest__.py
+++ spreadsheet_dashboard_oca/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Spreadsheet Dashboard Oca",
     "summary": """
         Use OCA Spreadsheets on dashboards configuration""",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.1.0",
     "license": "AGPL-3",
     "author": "CreuBlanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/spreadsheet",
@@ -14,9 +14,20 @@
         "spreadsheet_oca",
     ],
     "data": [
+        "security/ir.model.access.csv",
         "wizards/spreadsheet_spreadsheet_import.xml",
+        "wizards/spreadsheet_to_dashboard.xml",
         "views/spreadsheet_dashboard_group_views.xml",
         "views/spreadsheet_dashboard.xml",
         "data/spreadsheet_spreadsheet_import_mode.xml",
     ],
+    "assets": {
+        "spreadsheet.o_spreadsheet": [
+            (
+                "after",
+                "spreadsheet/static/src/o_spreadsheet/o_spreadsheet.js",
+                "spreadsheet_dashboard_oca/static/src/bundle/*.js",
+            ),
+        ],
+    },
 }
