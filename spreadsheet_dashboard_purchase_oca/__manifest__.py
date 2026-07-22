02e7ae206eeda7ef0bc5d8465ba14bf0816e5ca6
Pilar Vargas <pilar.vargas@tecnativa.com>
2026-07-22 10:02:29 +0200

    [MIG] spreadsheet_dashboard_purchase_oca: Migration to version 19.0
    TT61716

--- spreadsheet_dashboard_purchase_oca/__manifest__.py
+++ spreadsheet_dashboard_purchase_oca/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Spreadsheet dashboard for vendors",
     "category": "Hidden",
     "depends": ["spreadsheet_dashboard", "purchase"],
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "website": "https://github.com/OCA/spreadsheet",
     "author": "Odoo S.A., Tecnativa, Odoo Community Association (OCA)",
     "data": ["data/dashboards.xml"],
